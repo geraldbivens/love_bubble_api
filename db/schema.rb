217e4ea9df32e9e92fce3a28543ec615d8b17ed1
Gerald Bivens <geraldbivens@gmail.com>
2021-03-17 15:07:28 -0600

    Add username column to users table and run a migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_17_164130) do
+ActiveRecord::Schema.define(version: 2021_03_17_204054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_03_17_164130) do
     t.string "website"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
   end
 
 end
